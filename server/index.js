50ff39bd3f7761b3b72c65ca63a3053772864f56
tiffany <ting11222001@gmail.com>
2024-01-29 22:19:18 +1030

    feat: 🎸 set up connection to mongoDB and add seed data

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express"
+import bodyParser from "body-parser"
+import mongoose from "mongoose"
+import cors from "cors"
+import dotenv from "dotenv"
+import helmet from "helmet"
+import morgan from "morgan"
+
+// Configurations
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
+app.use(morgan("common"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+
+// check if the server is up
+console.log("hello!")
+
+
+// mongoose setup
+const PORT = process.env.PORT || 9000;
+mongoose
+.connect(process.env.MONGO_URL)
+.then(async() => {
+  app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+})
+.catch((error) => console.log(`${error} did not connect`))
