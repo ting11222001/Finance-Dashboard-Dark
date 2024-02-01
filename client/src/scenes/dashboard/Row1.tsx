8341cb9c3021ca1eca7a1a1b53ab3e0e9b54ad93
tiffany <ting11222001@gmail.com>
2024-02-02 00:30:09 +1030

    feat: 🎸 add area charts and line charts

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,8 +1,10 @@
+import BoxHeader from '@/components/BoxHeader';
 import DashboardBox from '@/components/DashboardBox'
 import { useGetKpisQuery } from '@/state/api'
-import { useTheme } from '@emotion/react';
+import { useTheme } from '@mui/material';
+
 import { useMemo } from 'react';
-import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { Area, AreaChart, CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Row1 = () => {
   const { palette } = useTheme();
@@ -15,17 +17,43 @@ const Row1 = () => {
       data[0].monthlyData.map(({ month, revenue, expenses }) => (
         {
           name: month.substring(0, 3),
-          revenue: revenue,
-          expenses: expenses
+          revenue: (revenue / 100).toFixed(2),
+          expenses: (expenses / 100).toFixed(2),
+          // name: month,
+          // revenue: revenue,
+          // expenses: expenses,
         })
       )
     )
   }, [data]);
   console.log("🚀 ~ revenueExpenses ~ revenueExpenses:", revenueExpenses)
 
+
+  const revenueProfit = useMemo(() => {
+    return (
+      data &&
+      data[0].monthlyData.map(({ month, revenue, expenses }) => (
+        {
+          name: month.substring(0, 3),
+          revenue: (revenue / 100).toFixed(2),
+          profit: (revenue / 100 - expenses / 100).toFixed(2),
+          // name: month.substring(0, 3),
+          // revenue: revenue,
+          // profit: revenue - expenses,
+        })
+      )
+    )
+  }, [data]);
+  console.log("🚀 ~ revenueProfit ~ revenueProfit:", revenueProfit)
+
   return (
     <>
       <DashboardBox bgcolor="#fff" gridArea="a">
+        <BoxHeader
+          title="Revenue and Expenses"
+          subtitle="top line represents revenue. Bottom line represents expenses."
+          sideText="+4%"
+        />
         <ResponsiveContainer width="100%" height="100%">
           <AreaChart
             width={500}
@@ -104,7 +132,67 @@ const Row1 = () => {
           </AreaChart>
         </ResponsiveContainer>
       </DashboardBox>
-      <DashboardBox bgcolor="#fff" gridArea="b"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="b">
+        <BoxHeader
+          title="Profit and Revenue"
+          subtitle="top line represents revenue. Bottom line represents profit."
+          sideText="+4%"
+        />
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            width={500}
+            height={400}
+            data={revenueProfit}
+            margin={{
+              top: 20,
+              right: 0,
+              left: -10,
+              bottom: 55,
+            }}
+          >
+            <CartesianGrid
+              vertical={false}
+              stroke={palette.grey[800]}
+            />
+            <XAxis dataKey="name"
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <YAxis
+              yAxisId="left"
+              tickLine={false}
+              axisLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <YAxis
+              yAxisId="right"
+              orientation="right"
+              tickLine={false}
+              axisLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <Tooltip />
+            <Legend
+              height={20}
+              wrapperStyle={{
+                margin: "0 0 10px 0",
+              }}
+            />
+            <Line
+              yAxisId="left"
+              type="monotone"
+              dataKey="profit"
+              stroke={palette.tertiary[500]}
+            />
+            <Line
+              yAxisId="right"
+              type="monotone"
+              dataKey="revenue"
+              stroke={palette.primary.main}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </DashboardBox>
       <DashboardBox bgcolor="#fff" gridArea="c"></DashboardBox>
     </>
   )
